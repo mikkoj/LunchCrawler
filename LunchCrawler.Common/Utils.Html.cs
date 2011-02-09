521fc0c256a4404a5d2c247b2a4da994a32969e1
Mikko Junnila <mikko.junnila@gmail.com>
2011-02-10 01:37:50 +0200

    Merge

--- LunchCrawler.Common/Utils.Html.cs
+++ LunchCrawler.Common/Utils.Html.cs
@@ -39,7 +39,7 @@ namespace LunchCrawler.Common
 
         public static LunchMenuDocument GetLunchMenuDocumentForUrl(string url)
         {
-            return GetLunchMenuDocumentForUrl(url, 10000);
+            return GetLunchMenuDocumentForUrl(url, 10);
         }
 
         public static LunchMenuDocument GetLunchMenuDocumentForUrl(string url, int timeout)
@@ -53,7 +53,7 @@ namespace LunchCrawler.Common
             try
             {
                 var request = (HttpWebRequest)WebRequest.Create(GetUri(url));
-                request.Timeout = timeout;
+                request.Timeout = timeout * 1000;
                 using (var response = (HttpWebResponse)request.GetResponse())
                 {
                     var headerEncoding = TryGetEncoding(response.ContentEncoding) ?? TryGetEncoding(response.CharacterSet) ?? Encoding.UTF8;
